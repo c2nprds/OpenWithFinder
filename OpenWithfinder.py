25c200ee6c3aa85f09d31b9b54eb2f722556d1d6
Yokoyama Daisuke <c2nprds@gmail.com>
2012-11-04 09:06:13 +0900

    add OpenWithFinder

---
+++ OpenWithfinder.py
@@ -0,0 +1,16 @@
+import sublime, sublime_plugin
+import os
+
+class OpenWithFinderCommand(sublime_plugin.WindowCommand):
+  def run(self):
+    import subprocess
+
+    if self.window.active_view():
+      subprocess.call(['open', '-R', self.window.active_view().file_name()])
+    else:
+      sublime.error_message(__name__ + ": No file to open.")
+      return
+
+
+  def is_visible(self):
+    return sublime.platform() == 'osx'
