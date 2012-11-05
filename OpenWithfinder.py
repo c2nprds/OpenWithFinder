308b602eefa4c39805d805be29ffc634b9025ced
l-liroki <l-liroki@live.com>
2012-11-05 15:22:22 +0900

    Update OpenWithfinder.py

--- OpenWithfinder.py
+++ OpenWithfinder.py
@@ -2,13 +2,8 @@ import sublime, sublime_plugin
 import os
 
 class OpenWithFinderCommand(sublime_plugin.WindowCommand):
-  def run(self, paths = [], application = "", extensions = ""):
-    import sys
+  def run(self):
     import subprocess
-    application_dir, application_name = os.path.split(application)
-    application_dir  = application_dir.encode(sys.getfilesystemencoding())
-    application_name = application_name.encode(sys.getfilesystemencoding())
-    application      = application.encode(sys.getfilesystemencoding())
 
     if self.window.active_view():
 
@@ -19,12 +14,6 @@ class OpenWithFinderCommand(sublime_plugin.WindowCommand):
       elif sublime.platform() == 'windows':
         # windows
         subprocess.Popen(r'explorer /select, "' + self.window.active_view().file_name() + '"')
-      else:
-        # linux
-        # subprocess.call(['open', '-R', self.window.active_view().file_name()])
-        return
-
 
     else:
       sublime.error_message(__name__ + ": No file to open.")
-      return
